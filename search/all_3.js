4bd5c61b1e500ed8b158b81757ec322eaa3c1570
simonefeola <73527988+simonefeola@users.noreply.github.com>
2023-12-14 12:52:45 +0100

    Add files via upload

---
+++ search/all_3.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['elim_0',['elim',['../class_calendario.html#aa1de8b4fdc10da4cc4b8d225873160ba',1,'Calendario']]],
+  ['email_1',['email',['../struct_cliente.html#a129418d677fd44e12bd483b3a160f71f',1,'Cliente']]],
+  ['empty_2',['empty',['../class_calendario.html#a183fd4bd751e6cf0f18a67ebfb22d9e9',1,'Calendario']]]
+];
---
+++ search/functions_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['elim_0',['elim',['../class_calendario.html#aa1de8b4fdc10da4cc4b8d225873160ba',1,'Calendario']]],
+  ['empty_1',['empty',['../class_calendario.html#a183fd4bd751e6cf0f18a67ebfb22d9e9',1,'Calendario']]]
+];
